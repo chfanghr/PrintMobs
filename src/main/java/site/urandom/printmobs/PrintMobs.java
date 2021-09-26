c59681b7019b28960ee500810adc943b667e43e5
Hongrui Fang <chfanghr@gmail.com>
2021-09-26 18:24:09 +0800

    feat: add the ability to scan mobs player current world and all worlds

--- src/main/java/site/urandom/printmobs/PrintMobs.java
+++ src/main/java/site/urandom/printmobs/PrintMobs.java
@@ -1,8 +1,10 @@
 package site.urandom.printmobs;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Chunk;
 import org.bukkit.World;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -15,7 +17,60 @@ public final class PrintMobs extends JavaPlugin {
     private static final int mobTypeIdBegin = EntityType.CREEPER.getTypeId();
     private static final int defaultChunkRadius = 2;
 
-    private void ScanChunks(Player player, int radius) {
+    private static class ScanResult {
+        public final int totalNumber;
+        public final HashMap<EntityType, Integer> mobs;
+
+        public ScanResult(Collection<Entity> entities) {
+            mobs = new HashMap<>();
+            AtomicInteger count = new AtomicInteger();
+
+            entities.forEach(e -> {
+                EntityType type = e.getType();
+                if (type.getTypeId() >= mobTypeIdBegin) {
+                    if (mobs.containsKey(type)) {
+                        mobs.put(type, mobs.get(type) + 1);
+                    } else {
+                        mobs.put(type, 1);
+                    }
+                    count.addAndGet(1);
+                }
+            });
+
+            this.totalNumber = count.get();
+        }
+
+        private ScanResult(int totalNumber, HashMap<EntityType, Integer> mobs) {
+            this.totalNumber = totalNumber;
+            this.mobs = mobs;
+        }
+
+        public void sendResult(CommandSender sender, String header) {
+            sender.sendMessage(String.format(ChatColor.YELLOW + "%s: %d", header, totalNumber));
+            mobs.forEach((type, count)
+                    -> sender.sendMessage(String.format(ChatColor.GRAY + "* %s: %d", type.name(), count)));
+        }
+
+        public static ScanResult concat(Collection<ScanResult> results) {
+            int totalNumber = 0;
+            HashMap<EntityType, Integer> finalMobs = new HashMap<>();
+
+            for (ScanResult result : results) {
+                totalNumber += result.totalNumber;
+                result.mobs.forEach((k,v) -> {
+                   if(finalMobs.containsKey(k)){
+                       finalMobs.put(k, finalMobs.get(k)+v);
+                   }else{
+                       finalMobs.put(k, v);
+                   }
+                });
+            }
+
+            return new ScanResult(totalNumber, finalMobs);
+        }
+    }
+
+    private void scanChunks(Player player, int radius) {
         Chunk currentChunk = player.getLocation().getChunk();
         Collection<Chunk> nearbyChunks = nearbyChunks(currentChunk, radius);
 
@@ -25,25 +80,9 @@ public final class PrintMobs extends JavaPlugin {
             entities.addAll(Arrays.asList(chunk.getEntities()));
         }
 
-        HashMap<EntityType, Integer> mobs = new HashMap<>();
+        ScanResult result = new ScanResult(entities);
 
-        AtomicInteger totalNumber = new AtomicInteger();
-
-        entities.forEach(e -> {
-            EntityType type = e.getType();
-            if (type.getTypeId() >= mobTypeIdBegin) {
-                if (mobs.containsKey(type)) {
-                    mobs.put(type, mobs.get(type) + 1);
-                } else {
-                    mobs.put(type, 1);
-                }
-                totalNumber.addAndGet(1);
-            }
-        });
-
-        player.sendMessage(String.format(ChatColor.YELLOW + "In nearby chunks(radius=%d): %d", radius, totalNumber.get()));
-
-        mobs.forEach((type, count) -> player.sendMessage(String.format(ChatColor.GRAY + "* %s : %d", type.name(), count)));
+        result.sendResult(player, String.format("Mobs in nearby chunk(s)(radius=%d)", radius));
     }
 
     public static Collection<Chunk> nearbyChunks(Chunk origin, int radius) {
@@ -67,21 +106,53 @@ public final class PrintMobs extends JavaPlugin {
         return chunks;
     }
 
+    private void scanCurrentWorld(Player player) {
+        World world = player.getWorld();
+        Collection<Entity> entities = world.getEntities();
+
+        ScanResult result = new ScanResult(entities);
+
+        result.sendResult(player, String.format("Mobs in current world(world=%s)", world.getName()));
+    }
+
+    private void scanAllWorlds(CommandSender sender) {
+        List<ScanResult> results = new ArrayList<>();
+
+        for (World world: Bukkit.getWorlds()) {
+            results.add(new ScanResult(world.getEntities()));
+        }
+
+        ScanResult finalResult = ScanResult.concat(results);
+
+        finalResult.sendResult(sender, "Mobs in all worlds");
+    }
 
     public void onEnable() {
         Objects.requireNonNull(getCommand("pm"))
                 .setExecutor((sender, command, label, args) -> {
+                    if (args.length > 0 && args[0].equals("all")){
+                        scanAllWorlds(sender);
+                        return true;
+                    }
+
                     if (!(sender instanceof Player)) {
                         sender.sendMessage(ChatColor.RED + "This command can only be executed by player!");
                         return false;
                     }
 
+                    Player player = (Player) sender;
+
+                    if (args.length > 0 && args[0].equals("world")){
+                        scanCurrentWorld(player);
+                        return true;
+                    }
+
                     int radius = defaultChunkRadius;
 
-                    if(args.length > 0)
+                    if (args.length > 0)
                         radius = Integer.parseInt(args[0]);
 
-                    ScanChunks((Player) sender, radius);
+                    scanChunks(player, radius);
 
                     return true;
                 });
